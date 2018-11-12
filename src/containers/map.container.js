e259c5c5e17837b035bea93c7d1dd2e783207ea8
Stefan <stefan.v@fidenz.com>
2018-11-12 16:52:39 +0530

    Adds enable function logic

--- src/containers/map.container.js
+++ src/containers/map.container.js
@@ -1,22 +1,52 @@
+import _ from 'lodash';
 import { Container } from 'unstated';
 
 import { getData } from '../api/map.api';
 
 class MapContainer extends Container {
   state = {
-    data: null,
-    enableWeather: false,
-    enableTraffic: false,
-    enableFriction: false,
-    enableLaserData: false,
-    enableSpeedSigns: false,
-    enablePlowTrucks: false,
+    map: null,
+    toggleWeather: false,
+    toggleTraffic: false,
+    toggleFriction: false,
+    toggleLaserData: false,
+    toggleSpeedSigns: false,
+    togglePlowTrucks: false,
     weather: null,
     traffic: null,
   };
 
-  setEnable = (name) => {
-    this.setState({[name]: !this.state[name]});
+  setToggle = (name) => {
+    const stateName = name.slice(6).toLowerCase();
+
+    this.setState(
+      {[name]: !this.state[name]}, 
+      () => {
+        this.state[name] ? 
+          this.addToMap(this.state[stateName]) :
+          this.removeFromMap(this.state[stateName]);
+      });
+  };
+
+  addToMap = (data) => {
+    const { map } = this.state;
+    let updatedMap;
+
+    if (!_.isEmpty(map)) {
+      updatedMap = _.concat(map, data);
+    } else {
+      updatedMap = data;
+    }
+
+    this.setState({map: updatedMap}, () => {console.log(this.state.map)});
+  };
+  
+  removeFromMap = (data) => {
+    const { map } = this.state;
+    let updatedMap;
+    
+    updatedMap = _.filter(map, data);
+    this.setState({map: updatedMap}, () => {console.log(this.state.map)});
   };
 
   getData = async (component) => {
